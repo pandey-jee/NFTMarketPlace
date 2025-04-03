efa9f9dc82d079d32f4b325592459e8852201d62
pandey-jee <you@example.com>
2025-04-03 11:23:55 +0530

    first commit

---
+++ lib/data/nfts.ts
@@ -0,0 +1,165 @@
+export interface NFT {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    price: string;
+    creator: string;
+    category: string;
+}
+
+export const nfts: NFT[] = [
+    {
+        id: "1",
+        name: "Hand Drawn Ape #1",
+        description: "A unique hand-drawn NFT style ape illustration",
+        image: "https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149622021.jpg",
+        price: "0.5 ETH",
+        creator: "Artist1",
+        category: "Apes"
+    },
+    {
+        id: "2",
+        name: "Hand Drawn Ape #2",
+        description: "Another unique hand-drawn NFT style ape illustration",
+        image: "https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149611033.jpg",
+        price: "0.6 ETH",
+        creator: "Artist1",
+        category: "Apes"
+    },
+    {
+        id: "3",
+        name: "Galaxy Background",
+        description: "A beautiful gradient galaxy background",
+        image: "https://img.freepik.com/free-vector/gradient-galaxy-background_52683-140335.jpg",
+        price: "0.3 ETH",
+        creator: "Artist2",
+        category: "Backgrounds"
+    },
+    {
+        id: "4",
+        name: "Androgynous Avatar",
+        description: "A unique non-binary avatar character",
+        image: "https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100250.jpg",
+        price: "0.4 ETH",
+        creator: "Artist3",
+        category: "Avatars"
+    },
+    {
+        id: "5",
+        name: "Technology Illustration",
+        description: "A crazy style technology illustration",
+        image: "https://img.freepik.com/free-vector/crazy-style-technology-illustration_52683-93863.jpg",
+        price: "0.7 ETH",
+        creator: "Artist4",
+        category: "Technology"
+    },
+    {
+        id: "6",
+        name: "Frog Transformation",
+        description: "A unique frog transformation illustration",
+        image: "https://img.freepik.com/free-vector/frog-transformation_1196-453.jpg",
+        price: "0.45 ETH",
+        creator: "Artist5",
+        category: "Animals"
+    },
+    {
+        id: "7",
+        name: "Polygonal Face",
+        description: "A polygonal face with headphones",
+        image: "https://img.freepik.com/free-vector/polygonal-face-with-headphones_23-2147507024.jpg",
+        price: "0.55 ETH",
+        creator: "Artist6",
+        category: "Faces"
+    },
+    {
+        id: "8",
+        name: "Summer Ape",
+        description: "An ape wearing summer outfit",
+        image: "https://img.freepik.com/free-vector/ape-wearing-summer-outfit-character_43623-1541.jpg",
+        price: "0.65 ETH",
+        creator: "Artist1",
+        category: "Apes"
+    },
+    {
+        id: "9",
+        name: "Hand Drawn Ape #3",
+        description: "Another unique hand-drawn NFT style ape",
+        image: "https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149611045.jpg",
+        price: "0.5 ETH",
+        creator: "Artist1",
+        category: "Apes"
+    },
+    {
+        id: "10",
+        name: "Surrealist Galaxy",
+        description: "A gradient surrealist galaxy illustration",
+        image: "https://img.freepik.com/free-vector/gradient-surrealist-galaxy-illustration_52683-117908.jpg",
+        price: "0.4 ETH",
+        creator: "Artist2",
+        category: "Backgrounds"
+    },
+    {
+        id: "11",
+        name: "Surrealist Galaxy #2",
+        description: "Another gradient surrealist galaxy illustration",
+        image: "https://img.freepik.com/free-vector/gradient-surrealist-galaxy-illustration_52683-117885.jpg",
+        price: "0.4 ETH",
+        creator: "Artist2",
+        category: "Backgrounds"
+    },
+    {
+        id: "12",
+        name: "Crazy Universe",
+        description: "A crazy universe abstract illustration",
+        image: "https://img.freepik.com/free-vector/crazy-universe-abstract-illustration_52683-87571.jpg",
+        price: "0.6 ETH",
+        creator: "Artist4",
+        category: "Abstract"
+    },
+    {
+        id: "13",
+        name: "Fantasy Dwarf",
+        description: "A realistic fantasy dwarf illustration",
+        image: "https://img.freepik.com/free-vector/realistic-fantasy-illustration-dwarf-illustration_52683-95397.jpg",
+        price: "0.5 ETH",
+        creator: "Artist7",
+        category: "Fantasy"
+    },
+    {
+        id: "14",
+        name: "Space Illustration",
+        description: "A crazy style space illustration",
+        image: "https://img.freepik.com/free-vector/crazy-style-space-illustration_52683-95526.jpg",
+        price: "0.45 ETH",
+        creator: "Artist4",
+        category: "Space"
+    },
+    {
+        id: "15",
+        name: "Futuristic Humanoid",
+        description: "A portrait of a futuristic female humanoid",
+        image: "https://img.freepik.com/free-photo/portrait-futuristic-female-humanoid-with-advanced-technology_23-2151666311.jpg",
+        price: "0.7 ETH",
+        creator: "Artist8",
+        category: "Futuristic"
+    },
+    {
+        id: "16",
+        name: "Streetwear Illustration",
+        description: "A hand-drawn streetwear illustration",
+        image: "https://img.freepik.com/free-vector/hand-drawn-streetwear-illustration_23-2151254294.jpg",
+        price: "0.5 ETH",
+        creator: "Artist9",
+        category: "Fashion"
+    },
+    {
+        id: "17",
+        name: "Pop Art Technology",
+        description: "A hand-drawn pop art technology illustration",
+        image: "https://img.freepik.com/free-vector/hand-drawn-pop-art-technology-illustration_23-2149480714.jpg",
+        price: "0.6 ETH",
+        creator: "Artist4",
+        category: "Technology"
+    }
+]; 
